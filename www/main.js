2c9de48df73f22efc2f52ae871e2c71e5e5c07d1
saikat2006-cloud <saikatkhatua007@gmail.com>
2025-10-03 11:32:44 +0530

    add initial files

---
+++ www/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    // eel.init()()
+
+    $('.text').textillate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: "bounceIn",
+        },
+        out: {
+            effect: "bounceOut",
+        },
+
+    });
+});
+
